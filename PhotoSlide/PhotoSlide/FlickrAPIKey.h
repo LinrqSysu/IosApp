8dfa0481fd839c1c7c1551b5967a292f50244cf5
samuel <samuel@samueldeiMac.local>
2015-07-30 18:10:03 +0800

    fall

---
+++ PhotoSlide/PhotoSlide/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"7ee3a1803adac585a97652ab7ddeccb5"
---
+++ PhotoSlide/PhotoSlide/NotifyProtocol.h
@@ -0,0 +1,16 @@
+//
+//  NotifyProtocol.h
+//  PhotoSlide
+//
+//  Created by samuel on 15/7/29.
+//  Copyright (c) 2015年 samuel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DownloadNotify <NSObject>
+@optional
+
+-(void) notifyDownloadFinished:(NSError *)error;
+
+@end
